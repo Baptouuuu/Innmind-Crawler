18ce3b980fc39354489ad20c5b4fe5a098967b28
Baptiste Langlade <baptouuuu@gmail.com>
2014-10-22 20:23:11 +0200

    added representation of an html page

---
+++ src/Innmind/CrawlerBundle/Entity/HtmlPage.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Innmind\CrawlerBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+class HtmlPage extends Resource
+{
+    protected $alternates;
+    protected $author;
+    protected $description;
+    protected $canonical;
+    protected $webapp = false;
+    protected $title;
+    protected $content;
+    /** @var links found in the html */
+    protected $links;
+    protected $language;
+    protected $rss;
+
+    public function __construct()
+    {
+        $this->alternates = new ArrayCollection();
+        $this->links = new ArrayCollection();
+    }
+
+    public function addAlternate($uri)
+    {
+        $this->alternates->add((string) $uri);
+
+        return $this;
+    }
+
+    public function getAlternates()
+    {
+        return $this->alternates;
+    }
+
+    public function setAuthor($author)
+    {
+        $this->author = (string) $author;
+
+        return $this;
+    }
+
+    public function hasAuthor()
+    {
+        return (bool) $this->author;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function setDescription($desc)
+    {
+        $this->description = (string) $desc;
+
+        return $this;
+    }
+
+    public function hasDescription()
+    {
+        return (bool) $this->description;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setCanonical($uri)
+    {
+        $this->canonical = (string) $uri;
+
+        return $this;
+    }
+
+    public function hasCanonical()
+    {
+        return (bool) $this->canonical;
+    }
+
+    public function getCanonical()
+    {
+        return $this->canonical;
+    }
+
+    public function setWebApp()
+    {
+        $this->webapp = true;
+
+        return $this;
+    }
+
+    public function isWebApp()
+    {
+        return $this->webapp;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = (string) $title;
+
+        return $this;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    public function addLink($link)
+    {
+        $this->links->add($link);
+
+        return $this;
+    }
+
+    public function getLinks()
+    {
+        return $this->links;
+    }
+
+    public function setLanguage($language)
+    {
+        $this->language = $language;
+
+        return $this;
+    }
+
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+
+    public function setRSS($feed)
+    {
+        $this->rss = (string) $feed;
+
+        return $this;
+    }
+
+    public function hasRSS()
+    {
+        return (bool) $this->rss;
+    }
+
+    public function getRSS()
+    {
+        return $this->rss;
+    }
+}
---
+++ src/Innmind/CrawlerBundle/Tests/Entity/HtmlPageTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Innmind\CrawlerBundle\Tests\Entity;
+
+use Innmind\CrawlerBundle\Entity\HtmlPage;
+use Doctrine\Common\Collections\ArrayCollection;
+
+class HtmlPageTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAddAlternate()
+    {
+        $p = new HtmlPage();
+
+        $this->assertEquals($p->getAlternates()->count(), 0);
+        $this->assertEquals($p->addAlternate('http://localhost/en'), $p);
+        $this->assertEquals($p->getAlternates()->count(), 1);
+    }
+
+    public function testSetAuthor()
+    {
+        $p = new HtmlPage();
+
+        $this->assertEquals($p->hasAuthor(), false);
+        $this->assertEquals($p->setAuthor('myself'), $p);
+        $this->assertEquals($p->hasAuthor(), true);
+        $this->assertEquals($p->getAuthor(), 'myself');
+    }
+
+    public function testSetDescription()
+    {
+        $p = new HtmlPage();
+
+        $this->assertEquals($p->hasDescription(), false);
+        $this->assertEquals($p->setDescription('some content'), $p);
+        $this->assertEquals($p->hasDescription(), true);
+        $this->assertEquals($p->getDescription(), 'some content');
+    }
+
+    public function testSetCanonical()
+    {
+        $p = new HtmlPage();
+
+        $this->assertEquals($p->hasCanonical(), false);
+        $this->assertEquals($p->setCanonical('http://localhost/'), $p);
+        $this->assertEquals($p->hasCanonical(), true);
+        $this->assertEquals($p->getCanonical(), 'http://localhost/');
+    }
+
+    public function testSetWebApp()
+    {
+        $p = new HtmlPage();
+
+        $this->assertEquals($p->isWebApp(), false);
+        $this->assertEquals($p->setWebApp(), $p);
+        $this->assertEquals($p->isWebApp(), true);
+    }
+
+    public function testSetTitle()
+    {
+        $p = new HtmlPage();
+
+        $this->assertEquals($p->getTitle(), null);
+        $this->assertEquals($p->setTitle('title'), $p);
+        $this->assertEquals($p->getTitle(), 'title');
+    }
+
+    public function testSetContent()
+    {
+        $p = new HtmlPage();
+
+        $this->assertEquals($p->getContent(), null);
+        $this->assertEquals($p->setContent('Content text'), $p);
+        $this->assertEquals($p->getContent(), 'Content text');
+    }
+
+    public function testAddLinks()
+    {
+        $p = new HtmlPage();
+
+        $this->assertEquals($p->getLinks()->count(), 0);
+        $this->assertEquals($p->addLink('http://localhost/somewhere'), $p);
+        $this->assertEquals($p->getLinks()->count(), 1);
+    }
+
+    public function testSetLanguage()
+    {
+        $p = new HtmlPage();
+
+        $this->assertEquals($p->getLanguage(), null);
+        $this->assertEquals($p->setLanguage('fr-FR'), $p);
+        $this->assertEquals($p->getLanguage(), 'fr-FR');
+    }
+
+    public function testSetRSS()
+    {
+        $p = new HtmlPage();
+
+        $this->assertEquals($p->hasRSS(), false);
+        $this->assertEquals($p->setRSS('http://localhost/rss'), $p);
+        $this->assertEquals($p->hasRSS(), true);
+        $this->assertEquals($p->getRSS(), 'http://localhost/rss');
+    }
+}
