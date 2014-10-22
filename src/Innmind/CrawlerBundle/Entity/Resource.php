a67cacb0d263ed4a601e6504859df4eaae5c1740
Baptiste Langlade <baptouuuu@gmail.com>
2014-10-22 19:47:19 +0200

    added basic representation of a resource

---
+++ src/Innmind/CrawlerBundle/Entity/Resource.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Innmind\CrawlerBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Basic representation of a web resource
+ */
+class Resource
+{
+    protected $uri;
+    protected $scheme;
+    protected $host;
+    protected $domain;
+    protected $tld;
+    protected $port;
+    protected $url = '/';
+    protected $query;
+    protected $hash;
+    protected $headers;
+
+    public function __construct()
+    {
+        $this->headers = new ArrayCollection();
+    }
+
+    public function setURI($uri)
+    {
+        $this->uri = (string) $uri;
+
+        return $this;
+    }
+
+    public function getURI()
+    {
+        return $this->uri;
+    }
+
+    public function setScheme($scheme)
+    {
+        $this->scheme = (string) $scheme;
+
+        return $this;
+    }
+
+    public function getScheme()
+    {
+        return $this->scheme;
+    }
+
+    public function setHost($host)
+    {
+        $this->host = (string) $host;
+
+        return $this;
+    }
+
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    public function setDomain($domain)
+    {
+        $this->domain = (string) $domain;
+
+        return $this;
+    }
+
+    public function getDomain()
+    {
+        return $this->domain;
+    }
+
+    public function setTopLevelDomain($tld)
+    {
+        $this->tld = (string) $tld;
+
+        return $this;
+    }
+
+    public function getTopLevelDomain()
+    {
+        return $this->tld;
+    }
+
+    public function setPort($port)
+    {
+        $this->port = (int) $port;
+
+        return $this;
+    }
+
+    public function hasPort()
+    {
+        return is_int($this->port);
+    }
+
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    public function setURL($url)
+    {
+        $this->url = (string) $url;
+
+        return $this;
+    }
+
+    public function getURL()
+    {
+        return $this->url;
+    }
+
+    public function setQuery($query)
+    {
+        $this->query = (string) $query;
+
+        return $this;
+    }
+
+    public function hasQuery()
+    {
+        return (bool) $this->query;
+    }
+
+    public function getQuery()
+    {
+        return $this->query;
+    }
+
+    public function setHash($hash)
+    {
+        $this->hash = (string) $hash;
+
+        return $this;
+    }
+
+    public function hasHash()
+    {
+        return (bool) $this->hash;
+    }
+
+    public function getHash()
+    {
+        return $this->hash;
+    }
+
+    public function addHeader($key, $value)
+    {
+        $this->headers->set($key, $value);
+
+        return $this;
+    }
+
+    public function hasHeader($key)
+    {
+        return $this->headers->containsKey($key);
+    }
+
+    public function getHeader($key)
+    {
+        return $this->headers->get($key);
+    }
+
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+}
---
+++ src/Innmind/CrawlerBundle/Tests/Entity/ResourceTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Innmind\CrawlerBundle\Tests\Entity;
+
+use Innmind\CrawlerBundle\Entity\Resource;
+use Doctrine\Common\Collections\ArrayCollection;
+
+class ResourceTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSetURI()
+    {
+        $r = new Resource();
+
+        $this->assertEquals($r->getUri(), null);
+        $this->assertEquals($r->setUri('foo'), $r);
+        $this->assertEquals($r->getUri(), 'foo');
+    }
+
+    public function testSetScheme()
+    {
+        $r = new Resource();
+
+        $this->assertEquals($r->getScheme(), null);
+        $this->assertEquals($r->setScheme('http'), $r);
+        $this->assertEquals($r->getScheme(), 'http');
+    }
+
+    public function testSetHost()
+    {
+        $r = new Resource();
+
+        $this->assertEquals($r->getHost(), null);
+        $this->assertEquals($r->setHost('innmind.io'), $r);
+        $this->assertEquals($r->getHost(), 'innmind.io');
+    }
+
+    public function testSetDomain()
+    {
+        $r = new Resource();
+
+        $this->assertEquals($r->getDomain(), null);
+        $this->assertEquals($r->setDomain('innmind'), $r);
+        $this->assertEquals($r->getDomain(), 'innmind');
+    }
+
+    public function testSetTopLevelDomain()
+    {
+        $r = new Resource();
+
+        $this->assertEquals($r->getTopLevelDomain(), null);
+        $this->assertEquals($r->setTopLevelDomain('io'), $r);
+        $this->assertEquals($r->getTopLevelDomain(), 'io');
+    }
+
+    public function testSetPort()
+    {
+        $r = new Resource();
+
+        $this->assertEquals($r->hasPort(), false);
+        $this->assertEquals($r->setPort('80'), $r);
+        $this->assertEquals($r->hasPort(), true);
+        $this->assertEquals($r->getPort(), 80);
+    }
+
+    public function testSetURL()
+    {
+        $r = new Resource();
+
+        $this->assertEquals($r->getURL(), '/');
+        $this->assertEquals($r->setURL('/foo/bar'), $r);
+        $this->assertEquals($r->getURL(), '/foo/bar');
+    }
+
+    public function testSetHash()
+    {
+        $r = new Resource();
+
+        $this->assertEquals($r->hasHash(), false);
+        $this->assertEquals($r->setHash('foo'), $r);
+        $this->assertEquals($r->hasHash(), true);
+        $this->assertEquals($r->getHash(), 'foo');
+    }
+
+    public function testSetHeader()
+    {
+        $r = new Resource();
+
+        $this->assertEquals($r->hasHeader('Content-Type'), false);
+        $this->assertEquals($r->addHeader('Content-Type', 'text/plain'), $r);
+        $this->assertEquals($r->hasHeader('Content-Type'), true);
+        $this->assertEquals($r->getHeader('Content-Type'), 'text/plain');
+        $this->assertEquals($r->getHeaders() instanceof ArrayCollection, true);
+    }
+}
