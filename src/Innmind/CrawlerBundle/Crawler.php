74b9d7c6aec87fc4735358529951c257323ae633
Baptiste Langlade <baptouuuu@gmail.com>
2014-10-23 07:51:18 +0200

    added logger in crawler + splitted fat constructor in setters

--- src/Innmind/CrawlerBundle/Crawler.php
+++ src/Innmind/CrawlerBundle/Crawler.php
@@ -6,6 +6,7 @@ use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler as DomCrawler;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Validator\ValidatorInterface;
+use Psr\Log\LoggerInterface;
 use RuntimeException;
 use Innmind\CrawlerBundle\Event\ResourceEvent;
 use Innmind\CrawlerBundle\Event\ResourceEvents;
@@ -16,13 +17,31 @@ class Crawler
     protected $dispatcher;
     protected $factory;
     protected $validator;
+    protected $logger;
 
-    public function __construct(EventDispatcherInterface $dispatcher, ResourceFactory $factory, ValidatorInterface $validator)
+    public function __construct()
     {
         $this->client = new Client();
+    }
+
+    public function setDispatcher(EventDispatcherInterface $dispatcher)
+    {
         $this->dispatcher = $dispatcher;
+    }
+
+    public function setResourceFactory(ResourceFactory $factory)
+    {
         $this->factory = $factory;
+    }
+
+    public function setValidator(ValidatorInterface $validator)
+    {
         $this->validator = $validator;
+    }
+
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
     }
 
     public function crawl(ResourceRequest $request)
