0e26ecd6a65c06e65c348d27867a16c6a707e88b
Baptiste Langlade <baptouuuu@gmail.com>
2014-10-23 13:51:43 +0200

    added a pass to check if page is a web app

---
+++ src/Innmind/CrawlerBundle/Parser/WebAppPass.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Innmind\CrawlerBundle\Parser;
+
+use Innmind\CrawlerBundle\Event\ResourceEvent;
+use Innmind\CrawlerBundle\Entity\HtmlPage;
+
+class WebAppPass
+{
+    public function handle(ResourceEvent $event)
+    {
+        $resource = $event->getResource();
+
+        if (!($resource instanceof HtmlPage)) {
+            return;
+        }
+
+        $dom = $event->getDOM();
+        $link = $dom->filter('link[rel="manifest"][href]');
+
+        if ($link->count() === 1) {
+            $resource->setWebApp();
+        }
+    }
+}
---
+++ src/Innmind/CrawlerBundle/Tests/Parser/WebAppPassTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Innmind\CrawlerBundle\Tests\Parser;
+
+use Innmind\CrawlerBundle\Parser\WebAppPass;
+use Innmind\CrawlerBundle\Event\ResourceEvent;
+use Innmind\CrawlerBundle\Entity\Resource;
+use Innmind\CrawlerBundle\Entity\HtmlPage;
+use GuzzleHttp\Message\Response;
+use Symfony\Component\DomCrawler\Crawler;
+
+class WebAppPassTest extends \PHPUnit_Framework_TestCase
+{
+    protected $pass;
+
+    public function setUp()
+    {
+        $this->pass = new WebAppPass();
+    }
+
+    public function testDoesNotHandleIfNotHtmlPage()
+    {
+        $event = new ResourceEvent(new Resource, new Response(200), new Crawler);
+
+        try {
+            $this->pass->handle($event);
+            $this->assertTrue(true);
+        } catch (\Exception $e) {
+            $this->assertTrue(false, 'Should not throw if handling raw resource');
+        }
+    }
+
+    public function testDoesNotSetWebApp()
+    {
+        $event = new ResourceEvent(new HtmlPage, new Response(200), new Crawler);
+
+        $this->pass->handle($event);
+
+        $this->assertEquals($event->getResource()->isWebApp(), false);
+    }
+
+    public function testSetWebApp()
+    {
+        $dom = new Crawler;
+        $dom->addContent('<html><head><link rel="manifest" href="manifest.json"></head></html>');
+        $event = new ResourceEvent(new HtmlPage, new Response(200), $dom);
+
+        $this->pass->handle($event);
+
+        $this->assertEquals($event->getResource()->isWebApp(), true);
+    }
+}
