cf4206026f13d7c86972397263a5ce7a7f1187d9
Baptiste Langlade <baptouuuu@gmail.com>
2014-10-25 17:34:34 +0200

    added a pass to extract document description

---
+++ src/Innmind/CrawlerBundle/Parser/DescriptionPass.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Innmind\CrawlerBundle\Parser;
+
+use Innmind\CrawlerBundle\Event\ResourceEvent;
+use Innmind\CrawlerBundle\Entity\HtmlPage;
+
+/**
+ * Extract document description off of meta tag (if any)
+ * otherwise take the first 150 characters of page content
+ */
+class DescriptionPass
+{
+    public function handle(ResourceEvent $event)
+    {
+        $resource = $event->getResource();
+
+        if (!($resource instanceof HtmlPage)) {
+            return;
+        }
+
+        $meta = $event
+            ->getDOM()
+            ->filter('meta[name="description"][content]');
+
+        if ($meta->count() === 1) {
+            $desc = $meta->attr('content');
+        }
+
+        if (!isset($desc)) {
+            $desc = substr($resource->getContent(), 0, 150) . '...';
+        }
+
+        $resource->setDescription($desc);
+    }
+}
---
+++ src/Innmind/CrawlerBundle/Tests/Parser/DescriptionPassTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Innmind\CrawlerBundle\Tests\Parser;
+
+use Innmind\CrawlerBundle\Parser\DescriptionPass;
+use Innmind\CrawlerBundle\Entity\Resource;
+use Innmind\CrawlerBundle\Entity\HtmlPage;
+use Innmind\CrawlerBundle\Event\ResourceEvent;
+use GuzzleHttp\Message\Response;
+use Symfony\Component\DomCrawler\Crawler;
+
+class DescriptionPassTest extends \PHPUnit_Framework_TestCase
+{
+    protected $pass;
+
+    public function setUp()
+    {
+        $this->pass = new DescriptionPass;
+    }
+
+    public function testDoesNotHandleIfNotHtmlPage()
+    {
+        $event = new ResourceEvent(new Resource, new Response(200), new Crawler);
+
+        try {
+            $this->pass->handle($event);
+            $this->assertTrue(true);
+        } catch (\Exception $e) {
+            $this->assertTrue(false, 'Should not throw if handling raw resource');
+        }
+    }
+
+    public function testSetDescriptionViaMetaTag()
+    {
+        $dom = new Crawler;
+        $dom->addContent('<!DOCTYPE html>
+            <html>
+                <head>
+                    <meta name="description" content="Test">
+                </head>
+            </html>');
+        $event = new ResourceEvent(new HtmlPage, new Response(200), $dom);
+
+        $this->pass->handle($event);
+
+        $this->assertEquals($event->getResource()->getDescription(), 'Test');
+    }
+
+    public function testSetDescriptionViaContent()
+    {
+        $dom = new Crawler;
+        $dom->addContent('<!DOCTYPE html><html></html>');
+        $resource = new HtmlPage;
+        $resource->setContent('This project is an attempt to build a search engine that helps you discover new things.
+    The prime goal is to offer results based on your search, but instead of redirecting you
+    to the website, Innmind will also propose content related to the one you\'re about to reach.');
+        $event = new ResourceEvent($resource, new Response(200), $dom);
+
+        $this->pass->handle($event);
+
+        $this->assertEquals($event->getResource()->getDescription(), 'This project is an attempt to build a search engine that helps you discover new things.
+    The prime goal is to offer results based on your search, b...');
+    }
+}
